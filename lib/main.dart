3f990523846b53cce341243943843976a573f9c9
rahul kathait <edendattox@gmail.com>
2022-04-12 11:19:48 +0530

    Created template

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Codepur";
+
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
